ff3b249e8791131ebfae02b3a592cd30605af955
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-12 19:32:43 +0300

    Create User model (inherits AbstactUser) with custom UserManager & create UserFollowing model for following/followers system

--- user/models.py
+++ user/models.py
@@ -1,3 +1,80 @@
 from django.db import models
+from django.contrib.auth.models import (
+    AbstractUser,
+    BaseUserManager,
+)
+from django.utils.translation import gettext as _
+from django.contrib.auth.validators import UnicodeUsernameValidator
 
-# Create your models here.
+from social_media_api.settings import AUTH_USER_MODEL
+
+
+class UserManager(BaseUserManager):
+    """Define a model manager for User model with no username field."""
+
+    use_in_migrations = True
+
+    def _create_user(self, email, password, **extra_fields):
+        """Create and save a User with the given email and password."""
+        if not email:
+            raise ValueError("The given email must be set")
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
+        user.set_password(password)
+        user.save(using=self._db)
+        return user
+
+    def create_user(self, email, password=None, **extra_fields):
+        """Create and save a regular User with the given email and password."""
+        extra_fields.setdefault("is_staff", False)
+        extra_fields.setdefault("is_superuser", False)
+        return self._create_user(email, password, **extra_fields)
+
+    def create_superuser(self, email, password, **extra_fields):
+        """Create and save a SuperUser with the given email and password."""
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
+
+        if extra_fields.get("is_staff") is not True:
+            raise ValueError("Superuser must have is_staff=True.")
+        if extra_fields.get("is_superuser") is not True:
+            raise ValueError("Superuser must have is_superuser=True.")
+
+        return self._create_user(email, password, **extra_fields)
+
+
+class User(AbstractUser):
+
+    username_validator = UnicodeUsernameValidator()
+
+    username = models.CharField(
+        _("username"),
+        max_length=150,
+        unique=False,
+        validators=[username_validator],
+        error_messages={
+            "unique": _("A user with that username already exists."),
+        },
+    )
+    email = models.EmailField(_("email address"), unique=True)
+
+    USERNAME_FIELD = "email"
+    REQUIRED_FIELDS = []
+
+    objects = UserManager()
+
+
+class UserFollowing(models.Model):
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        related_name="following",
+        on_delete=models.CASCADE
+    )
+    following_user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        related_name="followers",
+        on_delete=models.CASCADE
+    )
+
+    def __str__(self) -> str:
+        return "{self.user} follows {self.following_user}"
