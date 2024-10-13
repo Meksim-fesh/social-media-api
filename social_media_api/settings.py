7b5e10b655b2957776669c3a5444e0187cdb8f4d
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-13 14:48:19 +0300

    Add basic jwt settings

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+from datetime import timedelta
 from pathlib import Path
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -135,3 +136,9 @@ MEDIA_ROOT = BASE_DIR / "media"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+SIMPLE_JWT = {
+    "ACCESS_TOKEN_LIFETIME": timedelta(weeks=5),
+    "REFRESH_TOKEN_LIFETIME": timedelta(weeks=10),
+}
