a7f1b4e39eb6d8e232c7be36e6b1140484cb92c4
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-13 19:23:06 +0300

    Add views for followers/following system

--- user/views.py
+++ user/views.py
@@ -4,6 +4,7 @@ from django.db.models import Count
 from rest_framework import generics
 from rest_framework.permissions import IsAuthenticated, AllowAny
 from rest_framework_simplejwt.authentication import JWTAuthentication
+from rest_framework.response import Response
 
 from user import serializers
 
@@ -49,3 +50,29 @@ class UserRetrieveView(generics.RetrieveAPIView):
             Count("followers")
         ),
     )
+
+
+class RetrieveUserFollowersView(generics.GenericAPIView):
+    serializer_class = serializers.UserFollowersListSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+    queryset = get_user_model().objects.prefetch_related("followers__user")
+
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object().followers.all()
+        serializer = self.get_serializer(instance, many=True)
+        return Response(serializer.data)
+
+
+class RetrieveUserFollowingsView(generics.GenericAPIView):
+    serializer_class = serializers.UserFollowingListSerializer
+    permission_classes = (IsAuthenticated,)
+    authentication_classes = (JWTAuthentication,)
+    queryset = get_user_model().objects.prefetch_related(
+        "following__following_user"
+    )
+
+    def get(self, request, *args, **kwargs):
+        instance = self.get_object().following.all()
+        serializer = self.get_serializer(instance, many=True)
+        return Response(serializer.data)
