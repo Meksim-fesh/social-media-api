f1ea308d024c2e44bae28fc6c07a6da801226c1d
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-22 01:48:01 +0300

    Create models Post, Like and Comment and register them in the admin panel

--- post/admin.py
+++ post/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
 
-# Register your models here.
+from post.models import Comment, Like, Post
+
+
+admin.site.register(Post)
+admin.site.register(Like)
+admin.site.register(Comment)
--- post/models.py
+++ post/models.py
@@ -1,3 +1,62 @@
-from django.db import models
+import os
+import uuid
 
-# Create your models here.
+from django.db import models
+from django.utils.text import slugify
+
+from social_media_api.settings import AUTH_USER_MODEL
+
+
+def post_file_path(instance, filename) -> os.path:
+    name, extension = os.path.splitext(filename)
+    name = f"{slugify(name)}-{slugify(instance.hashtag)}"
+    filename = f"{name}-{uuid.uuid4()}{extension}"
+    return os.path.join("uploads/posts/", filename)
+
+
+class Post(models.Model):
+    file = models.FileField(upload_to=post_file_path)
+    text = models.TextField()
+    hashtag = models.CharField(max_length=255)
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        related_name="posts",
+        on_delete=models.CASCADE,
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"Post by {self.user}"
+
+
+class Like(models.Model):
+    post = models.ForeignKey(
+        Post,
+        related_name="likes",
+        on_delete=models.CASCADE
+    )
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        related_name="likes",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"Like to {self.post}"
+
+
+class Comment(models.Model):
+    content = models.TextField()
+    post = models.ForeignKey(
+        Post,
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+    user = models.ForeignKey(
+        AUTH_USER_MODEL,
+        related_name="comments",
+        on_delete=models.CASCADE,
+    )
+
+    def __str__(self):
+        return f"Comment to {self.post}"
