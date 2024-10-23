fcb91fe51fac4dc1a8bb4c40baa4bd542ec523d4
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-23 22:32:26 +0300

    Add missing Media urls to urls.py

--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -17,6 +17,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
+
 from debug_toolbar.toolbar import debug_toolbar_urls
 
 
@@ -24,4 +27,6 @@ urlpatterns = [
     path("admin/", admin.site.urls),
     path("api/user/", include("user.urls", namespace="user")),
     path("api/post/", include("post.urls", namespace="post")),
-] + debug_toolbar_urls()
+] + debug_toolbar_urls() + static(
+    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+)
