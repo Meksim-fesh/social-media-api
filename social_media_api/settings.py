e8d2810abe08969ddd338122fc15c02a5bfd077d
Maksym Nimych <maksimnimitch@gmail.com>
2024-10-25 03:53:12 +0300

    Add swagger & redoc documentation and update requirements.txt

--- social_media_api/settings.py
+++ social_media_api/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
     "rest_framework_simplejwt",
     "debug_toolbar",
     "rest_framework_simplejwt.token_blacklist",
+    "drf_spectacular",
 ]
 
 MIDDLEWARE = [
@@ -143,6 +144,16 @@ MEDIA_ROOT = BASE_DIR / "media"
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+REST_FRAMEWORK = {
+    "DEFAULT_SCHEMA_CLASS": "drf_spectacular.openapi.AutoSchema",
+}
+
+SPECTACULAR_SETTINGS = {
+    "TITLE": "Social Media API",
+    "DESCRIPTION": "Simple Social Media API",
+    "VERSION": "1.0.0",
+    "SERVE_INCLUDE_SCHEMA": False,
+}
 
 SIMPLE_JWT = {
     "ACCESS_TOKEN_LIFETIME": timedelta(weeks=5),
--- social_media_api/urls.py
+++ social_media_api/urls.py
@@ -21,12 +21,30 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 from debug_toolbar.toolbar import debug_toolbar_urls
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView,
+)
 
 
-urlpatterns = [
-    path("admin/", admin.site.urls),
-    path("api/user/", include("user.urls", namespace="user")),
-    path("api/post/", include("post.urls", namespace="post")),
-] + debug_toolbar_urls() + static(
-    settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+urlpatterns = (
+    [
+        path("admin/", admin.site.urls),
+        path("api/user/", include("user.urls", namespace="user")),
+        path("api/post/", include("post.urls", namespace="post")),
+        path("api/doc/", SpectacularAPIView.as_view(), name="schema"),
+        path(
+            "api/doc/swagger/",
+            SpectacularSwaggerView.as_view(url_name="schema"),
+            name="swagger",
+        ),
+        path(
+            "api/doc/redoc/",
+            SpectacularRedocView.as_view(url_name="schema"),
+            name="redoc",
+        ),
+    ]
+    + debug_toolbar_urls()
+    + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 )
